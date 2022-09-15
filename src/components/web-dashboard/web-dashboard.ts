887e048bf507797390feeb8998f35a0bc534f1a0
john-mirage <chaosdynamix@posteo.net>
2022-09-15 12:18:52 +0200

    feat: work on lists

--- src/components/web-dashboard/web-dashboard.ts
+++ src/components/web-dashboard/web-dashboard.ts
@@ -1,10 +1,34 @@
+import WebSocialMedia from "@components/web-social-media/web-social-media";
+import WebStat from "@components/web-stat/web-stat";
+import data from "@data/social-medias.json";
+
 class WebDashboard extends HTMLDivElement {
+  socialMediasListElement: HTMLUListElement;
+  statsListElement: HTMLUListElement;
+  webSocialMedia: WebSocialMedia;
+  webStat: WebStat;
+  
   constructor() {
     super();
+    this.socialMediasListElement = <HTMLUListElement>this.querySelector('[data-id="web-dashboard-social-medias"]');
+    this.statsListElement = <HTMLUListElement>this.querySelector('[data-id="web-dashboard-stats"]');
+    this.webSocialMedia = <WebSocialMedia>document.createElement("li", { is: "web-social-media" });
+    this.webStat = <WebStat>document.createElement("li", { is: "web-stat" });
   }
 
   connectedCallback() {
-    
+    this.socialMediasListElement.replaceChildren();
+    this.statsListElement.replaceChildren();
+    data.forEach((socialMedia: AppData.SocialMedia) => {
+      const webSocialMedia = <WebSocialMedia>this.webSocialMedia.cloneNode(true);
+      webSocialMedia.socialMedia = socialMedia;
+      this.socialMediasListElement.append(webSocialMedia);
+      socialMedia.secondary.forEach((stat) => {
+        const webStat = <WebStat>this.webStat.cloneNode(true);
+        webStat.stat = stat;
+        this.statsListElement.append(webStat);
+      });
+    });
   }
 }
 
--- src/components/web-social-media/web-social-media.ts
+++ src/components/web-social-media/web-social-media.ts
@@ -1,10 +1,33 @@
-class WebSocialMedia extends HTMLElement {
+class WebSocialMedia extends HTMLLIElement {
+  #initialMount = true;
+  #templateFragment: DocumentFragment;
+  #socialMedia?: AppData.SocialMedia;
+
   constructor() {
     super();
+    const template = <HTMLTemplateElement>document.getElementById("template-web-social-media");
+    this.#templateFragment = <DocumentFragment>template.content.cloneNode(true);
+  }
+
+  get socialMedia(): AppData.SocialMedia {
+    if (this.#socialMedia) {
+      return this.#socialMedia;
+    } else {
+      throw new Error("The social media is not valid");
+    }
+  }
+
+  set socialMedia(newSocialMedia: AppData.SocialMedia) {
+    this.#socialMedia = newSocialMedia;
+    // TODO
   }
 
   connectedCallback() {
-    
+    if (this.#initialMount) {
+      this.classList.add("web-social-media");
+      this.append(this.#templateFragment);
+      this.#initialMount = false;
+    }
   }
 }
 
