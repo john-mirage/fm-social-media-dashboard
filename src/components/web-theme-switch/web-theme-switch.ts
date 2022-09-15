e3e94710d38872d8615e25a7d8007fe3e9f3c4d3
john-mirage <chaosdynamix@posteo.net>
2022-09-15 11:29:08 +0200

    feat: add theme switch

--- src/components/web-theme-switch/web-theme-switch.ts
+++ src/components/web-theme-switch/web-theme-switch.ts
@@ -1,10 +1,29 @@
+const LIGHT_THEME = "light";
+const DARK_THEME = "dark";
+
 class WebThemeSwitch extends HTMLLabelElement {
+  inputElement: HTMLInputElement;
+
   constructor() {
     super();
+    this.inputElement = <HTMLInputElement>this.querySelector('[data-id="web-theme-switch-input"]');
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   connectedCallback() {
-    
+    this.inputElement.addEventListener("click", this.handleInputChange);
+  }
+
+  disconnectedCallback() {
+    this.inputElement.removeEventListener("click", this.handleInputChange);
+  }
+
+  handleInputChange() {
+    const customEvent = new CustomEvent("update-theme", {
+      bubbles: true,
+      detail: { theme: this.inputElement.checked ? LIGHT_THEME : DARK_THEME }
+    });
+    this.dispatchEvent(customEvent);
   }
 }
 
