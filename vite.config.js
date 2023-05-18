6856e25351dea6e127f16bf861110bf860ed0207
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-18 22:02:02 +0200

    fix: remove base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/social-media-dashboard/",
   resolve: {
     alias: {
       '@components': resolve(__dirname, 'src/components'),
